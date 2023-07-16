e33d2c25dfb69b344985d510c861514597886ea1
Maciek <maciejfrs@gmail.com>
2023-07-16 12:58:22 +0100

    Changed directory structure

--- work-centre-ui/karma.conf.js
+++ work-centre-ui/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/work-centre'),
+      dir: require('path').join(__dirname, './coverage/work-centre-ui'),
       subdir: '.',
       reporters: [
         { type: 'html' },
