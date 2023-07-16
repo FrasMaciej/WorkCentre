84150ba900b0f36b0f01a9b2c68bd8832d69c8ab
Maciek <maciejfrs@gmail.com>
2023-07-16 23:15:08 +0100

    Express again

--- work-centre-api/src/app.ts
+++ work-centre-api/src/app.ts
@@ -1,33 +1,18 @@
-// import express from 'express';
-// import apiRouter from './api/routes/api';
+import express from 'express';
+import apiRouter from './api/routes/api';
 
-// import { constants } from './projectConfigurationConstants';
+import { constants } from './projectConfigurationConstants';
 
-// const app = express();
+const app = express();
 
-// app.get('/', (req, res) => {
-//     res.send('Hello World!');
-// });
-
-// app.listen(constants.server_port, () => {
-//     return console.log(`Express is listening on port ${constants.server_port}`);
-// });
-
-// app.use('/api', apiRouter);
-
-const http = require('http');
-
-const hostname = '127.0.0.1';
-const port = 3000;
-
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World');
+app.get('/', (req, res) => {
+    res.send('Hello World!');
 });
 
-server.listen(port, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+app.listen(constants.server_port, () => {
+    return console.log(`Express is listening on port ${constants.server_port}`);
 });
+
+app.use('/api', apiRouter);
 
 
