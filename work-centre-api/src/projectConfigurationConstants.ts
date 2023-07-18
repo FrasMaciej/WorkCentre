8ec0ddf34ae06eb0f8331f291da0c073888cd498
Maciek <maciejfrs@gmail.com>
2023-07-18 13:25:58 +0100

    Try to fix port problem on deploy

--- work-centre-api/src/projectConfigurationConstants.ts
+++ work-centre-api/src/projectConfigurationConstants.ts
@@ -3,5 +3,5 @@ import { config } from 'dotenv'
 config();
 
 export const constants = {
-    server_port: Number(process.env.SERVER_PORT)
+    server_port: Number(process.env.SERVER_PORT || 8080)
 }
