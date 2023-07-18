1bbc2afa19b0cad1496fb934b3689877f8ec8042
Maciek <maciejfrs@gmail.com>
2023-07-18 13:28:45 +0100

    Port changed

--- work-centre-api/src/projectConfigurationConstants.ts
+++ work-centre-api/src/projectConfigurationConstants.ts
@@ -3,5 +3,5 @@ import { config } from 'dotenv'
 config();
 
 export const constants = {
-    server_port: Number(process.env.SERVER_PORT || 8080)
+    server_port: Number(process.env.SERVER_PORT || 3000)
 }
