f77e37df0aada05524914d4a8c7f24c7b846a2db
FrasMaciej <37934674+FrasMaciej@users.noreply.github.com>
2023-07-19 15:36:48 +0200

    Update testActions.ts

--- work-centre-api/src/api/actions/testActions.ts
+++ work-centre-api/src/api/actions/testActions.ts
@@ -3,3 +3,7 @@ import { Request, Response } from 'express';
 export async function test(req: Request, res: Response) {
     return res.status(200).json({ message: "Api Works :)" })
 }
+
+export async function test2(req: Request, res: Response) {
+    return res.status(200).json({ message: "Api Works2 :)" })
+}
