e593a6389a25bec842de1dab4b8ccc374ea236fc
FrasMaciej <37934674+FrasMaciej@users.noreply.github.com>
2023-07-19 15:37:08 +0200

    Update api.ts

--- work-centre-api/src/api/routes/api.ts
+++ work-centre-api/src/api/routes/api.ts
@@ -4,7 +4,7 @@ import * as testActions from '../actions/testActions';
 const apiRouter = express.Router();
 
 apiRouter.get('/test', testActions.test);
-apiRouter.get('/test2', testActions.test);
+apiRouter.get('/test2', testActions.test2);
 
 
 export default apiRouter;
