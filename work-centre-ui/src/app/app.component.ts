af75f31ceae4ef892ed75a64af3b5aed9c3885d4
Maciek <maciejfrs@gmail.com>
2023-07-25 19:17:44 +0100

    Components changed to inline style, angular material + bootstrap added

--- work-centre-ui/src/app/app.component.ts
+++ work-centre-ui/src/app/app.component.ts
@@ -2,9 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: `
+  `,
+  styles: [`
+  `]
 })
 export class AppComponent {
-  title = 'work-centre-ui';
+
 }
+
+
