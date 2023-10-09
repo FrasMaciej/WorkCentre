ed32d0b1a209a7c2c016e7228c824307d057ca6c
Maciek <maciejfrs@gmail.com>
2023-10-09 23:27:17 +0100

    Some fixes

--- work-centre-ui/src/environments/environment.prod.ts
+++ work-centre-ui/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   title: 'Production Environment Heading',
   apiURL: 'https://star-jobs-api.azurewebsites.net/api',
-  serverURL: 'https://star-jobs-api.azurewebsites.net'
+  serverUrl: 'https://star-jobs-api.azurewebsites.net'
 };
